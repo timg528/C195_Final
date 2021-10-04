3b354a4bdb4592802caf15524a423abb5b70a52d
Tim Graham <timg528@gmail.com>
2021-10-04 19:59:46 +0000

    Got these two linked and the Exit button working

--- src/Controllers/Login.java
+++ src/Controllers/Login.java
@@ -1,4 +1,23 @@
 package Controllers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Login {
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button loginButton;
+    @FXML private Button exitButton;
+
+    @FXML
+    private void exitProgramButton(ActionEvent event) {
+        Platform.exit();
+    }
+
+
 }

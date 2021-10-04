39c60e1fccd0498c794d81f8d44f05dad34f09bd
Tim Graham <timg528@gmail.com>
2021-10-04 21:01:09 +0000

    Result set not working just yet. Looks like it's coming up empty.

--- src/Controllers/Login.java
+++ src/Controllers/Login.java
@@ -1,12 +1,16 @@
 package Controllers;
 
+import Helpers.DBConnection;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Login {
     @FXML private TextField usernameField;
@@ -20,7 +24,32 @@ public class Login {
     }
 
     @FXML
-    private void loginButton(ActionEvent event) {
+    private void loginButton(ActionEvent event) throws Exception {
+        final String username = usernameField.getText();
+        final String password = passwordField.getText();
+
+        String selectUser = "Select User_ID FROM users WHERE User_Name = '"+username+"' AND Password = '"+password+"'";
+
+        System.out.println(selectUser);
+
+        PreparedStatement ps = DBConnection.getConnection().prepareStatement(selectUser);
+
+
+        ResultSet rs = ps.executeQuery();
+
+        // Testing to learn Result Set
+        System.out.println(rs.getInt("User_ID"));
+
+
+
+        /**
+        if (rs.getInt("User_ID") != 1) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Invalid credentials");
+            errorAlert.showAndWait();
+        }
+         **/
+
 
     }
 

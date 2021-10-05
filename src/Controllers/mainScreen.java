7529c8e8221a8b319b2cea4d6862afbf25ce4535
Tim Graham <timg528@gmail.com>
2021-10-05 16:48:34 +0000

    Initial bits of the main screen view and controller

---
+++ src/Controllers/mainScreen.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class mainScreen {
+    /**
+     * Alright, so here we'll see all of our appointments in a tableview
+     */
+
+    @FXML private TableView appointmentTable;
+    @FXML private TableColumn appointmentIDColumn;
+    @FXML private TableColumn appointmentTitleColumn;
+    @FXML private TableColumn appointmentDescriptionColumn;
+    @FXML private TableColumn appointmentLocationColumn;
+    @FXML private TableColumn appointmentContactColumn;
+    @FXML private TableColumn appointmentTypeColumn;
+    @FXML private TableColumn appointmentStartColumn;
+    @FXML private TableColumn appointmentEndColumn;
+    @FXML private TableColumn appointmentCustomerIDColumn;
+    @FXML private TableColumn appointmentUserIDColumn;
+
+
+
+}

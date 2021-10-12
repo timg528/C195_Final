fa8733bcd97625d43865661c65fb9cc28df1bf5c
Tim Graham <timg528@gmail.com>
2021-10-12 18:59:07 +0000

    Created the startConnection method

--- src/Helpers/DBConnection.java
+++ src/Helpers/DBConnection.java
@@ -8,18 +8,31 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    //private static final String sqlDriver = "com.mysql.jdbc.Driver"; //Not sure if this is used
+    private static final String sqlDriver = "com.mysql.jdbc.Driver"; //Not sure if this is used
 
     private static final String dbName="client_schedule";
     private static final String dbURL="jdbc:mysql://localhost:3306/"+dbName;
     private static final String dbUser="sqlUser";
     private static final String dbPass="Passw0rd!";
 
-    static Connection conn;
+    public static Connection conn = null;
 
+    public static Connection startConnection() {
+        try {
+            Class.forName(sqlDriver);
+            conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
     // Manage the connection
     public static void openConnection() throws ClassNotFoundException, SQLException, Exception{
         conn=(Connection) DriverManager.getConnection(dbURL, dbUser, dbPass);
+
     }
 
     public static void closeConnection() throws ClassNotFoundException, SQLException, Exception {
@@ -28,7 +41,7 @@ public class DBConnection {
 
 
     // Get Connection
-    public static Connection getConnection() throws ClassNotFoundException, SQLException, Exception{
+    public static Connection getConnection() {
         return conn;
     }
 }

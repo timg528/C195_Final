f6b68097f2c5df974555c215d52405f3e6e1efa8
Tim Graham <timg528@gmail.com>
2021-11-03 00:47:52 +0000

    Updated user model and created UserDAO

---
+++ src/DAO/User/UserDAO.java
@@ -0,0 +1,30 @@
+package DAO.User;
+
+import Helpers.DBConnection;
+import Models.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+    public static ObservableList<User> getAllUsers() throws Exception {
+        String sql = "SELECT User_ID, User_Name from users";
+        ObservableList<User> allUsers = FXCollections.observableArrayList();
+
+        PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            int userId = rs.getInt("User_ID");
+            String userName = rs.getString("User_Name");
+
+            User u = new User(userId, userName);
+
+            allUsers.add(u);
+        }
+        return allUsers;
+    }
+}

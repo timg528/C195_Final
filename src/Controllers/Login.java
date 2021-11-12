ca4ba7c8e44c094dbf42baebd1564beb63700f31
Tim Graham <timg528@gmail.com>
2021-11-12 13:20:00 -0500

    Cleaning up the login controller

--- src/Controllers/Login.java
+++ src/Controllers/Login.java
@@ -2,7 +2,6 @@ package Controllers;
 
 import Helpers.DBConnection;
 import Models.Data;
-import Models.User;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,15 +22,13 @@ public class Login {
      *    - Accepts a user ID and password and provides an appropriate error message
      *    - Determines the user's location (i.e., ZoneId) and displays it in a label on the log-in form
      *    - Displays the log-in form in English or French based on the user's computer language setting to translate
-     *      all the text, labesl, buttons, and errors
+     *      all the text, labels, buttons, and errors
      *    - Automatically translates error control messages into English or French based on the user's comp language
      *    - (Unlisted) Only accepts the "test" user to login to the application
      *      - User: "test", Pass: "test", User_ID: 1
      */
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
-    @FXML private Button loginButton;
-    @FXML private Button exitButton;
     @FXML private Label systemTimezone;
 
     public void initialize() throws Exception {
@@ -42,56 +39,54 @@ public class Login {
 
     }
 
-
-
     @FXML
-    private void exitProgramButton(ActionEvent event) {
+    private void exitProgramButton() {
         Platform.exit();
     }
 
-    // I should separate out the credential checking here
-    private void checkCredentials() throws Exception {
-
-
-    }
 
     @FXML
     private void loginButton(ActionEvent event) throws Exception {
 
+        if (usernameField.getText().equals("test")) {
+            String selectUser = "Select User_ID FROM users WHERE User_Name = ? AND Password = ?";
 
-        final String username = usernameField.getText();
-        final String password = passwordField.getText();
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(selectUser);
 
-        String selectUser = "Select User_ID FROM users WHERE User_Name = '"+username+"' AND Password = '"+password+"'";
-
-        PreparedStatement ps = DBConnection.getConnection().prepareStatement(selectUser);
+            ps.setString(1, usernameField.getText());
+            ps.setString(2, passwordField.getText());
 
 
-        ResultSet rs = ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
 
-        if(rs.next()) {
-            User user = new User(rs.getInt("User_ID"), username);
-            System.out.println("User_ID = "+user.getId());
+            if(rs.next()) {
+                Data.setCurrentUser(rs.getInt("User_ID"));
+                Data.generateAll();
 
-            Data.setCurrentUser(user.getId());
-            Data.generateAll();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/mainScreen.fxml"));
+                mainScreen controller = new mainScreen();
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/mainScreen.fxml"));
-            mainScreen controller = new mainScreen();
+                loader.setController(controller);
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
 
-            loader.setController(controller);
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-
+            } else {
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("Invalid Credentials");
+                errorAlert.showAndWait();
+            }
         } else {
             Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setHeaderText("Invalid Credentials");
+            errorAlert.setHeaderText("Invalid Username");
+            errorAlert.setContentText("You must use test as the username");
             errorAlert.showAndWait();
         }
+
+
 
     }
 

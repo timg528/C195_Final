19b3676cbbd988df1cf9278ee5d6fbb50fa74853
Tim Graham <timg528@gmail.com>
2021-11-12 11:11:18 -0500

    Created class in order to handle finding all appointments by customer id

---
+++ src/Helpers/findAppointments.java
@@ -0,0 +1,18 @@
+package Helpers;
+
+import Models.Appointment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class findAppointments {
+
+    private static ObservableList<Appointment> getAppointmentsbyCustomer(int customerID) {
+        ObservableList<Appointment> appointments = FXCollections.observableArrayList();
+
+       // for (:
+        //     ) {
+
+       // }
+        return appointments;
+    }
+}
